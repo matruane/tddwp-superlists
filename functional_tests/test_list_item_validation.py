a40dd7aa0745b7c7ef85461c2f922efc0ea062f1
Mat Ruane <matr800@gmail.com>
2015-05-06 05:42:19 +1200

    add js and qunit test for clearing error messages on keypress

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -27,8 +27,7 @@ class ItemValidationTest(FunctionalTest):
 
         # She receives a similar warning on the list page
         self.check_for_row_in_list_table('1: Buy milk')
-        error = self.get_error_element() 
-        self.assertEqual(error.text, "You can't have an empty list item")
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # And she can correct it by filling some text in
@@ -55,16 +54,14 @@ class ItemValidationTest(FunctionalTest):
         # Edith starts a new list in a way that causes a validation error:
         self.browser.get(self.server_url)
         self.get_item_input_box().send_keys('\n')
-        error = self.get_error_element() 
-        self.assertEqual(error.text, "You can't have an empty list item")
+        error = self.get_error_element()
         self.assertTrue(error.is_displayed())
 
         # She starts typing in the input box to clear the error
         self.get_item_input_box().send_keys('a')
 
         # She is pleased to see that the error message disappears 
-        error = self.get_error_element() 
-        self.assertEqual(error.text, "You can't have an empty list item")
+        error = self.get_error_element()
         self.assertFalse(error.is_displayed())
 
 
